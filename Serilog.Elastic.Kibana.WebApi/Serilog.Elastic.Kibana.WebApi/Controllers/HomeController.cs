b5d80c41ea6693700ee8aca4437453833f1bdd7b
Narottam Goyal <narottamgoyal66@gmail.com>
2021-05-02 01:26:40 +0530

    initial code

---
+++ Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Serilog.Elastic.Kibana.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Working");
+        }
+    }
+}
