916ab43f5f17261bedf0dddda176c5be280fef1e
Narottam Goyal <narottamgoyal66@gmail.com>
2021-05-08 16:46:44 +0530

    Modified   Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Controllers/HomeController.cs Modified   Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Program.cs Modified   Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi.csproj Missing    Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/appsettings.Development.json Modified   Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/appsettings.json

--- Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Controllers/HomeController.cs
+++ Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Serilog.Elastic.Kibana.WebApi.Controllers
 {
@@ -18,7 +19,39 @@ namespace Serilog.Elastic.Kibana.WebApi.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+            var clientId = Faker.RandomNumber.Next(1, 1000);
+            _logger.LogInformation("Entering get method... {@clientId}", clientId);
+            try
+            {
+                for (int i = 3; i >= 0; i--)
+                {
+                    _logger.LogDebug("Custom object in log {@CustomerObj}", new Customer(clientId, Faker.Name.FullName(), Faker.Name.FullName()));
+                    DoSomething(i);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in Get... {@clientId}", clientId);
+            }
             return StatusCode(StatusCodes.Status200OK, "Working");
+        }
+
+        private static int DoSomething(int i)
+        {
+            return i / i;
+        }
+    }
+
+    public class Customer
+    {
+        public int ClientId { get; set; }
+        public string UserName { get; set; }
+        public string BusinessName { get; set; }
+        public Customer(int clientId, string userName, string businessName)
+        {
+            ClientId = clientId;
+            UserName = userName;
+            BusinessName = businessName;
         }
     }
 }
--- Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Program.cs
+++ Serilog.Elastic.Kibana.WebApi/Serilog.Elastic.Kibana.WebApi/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
 
 namespace Serilog.Elastic.Kibana.WebApi
 {
@@ -7,7 +12,29 @@ namespace Serilog.Elastic.Kibana.WebApi
     {
         public static void Main(string[] args)
         {
+            ConfigureSerilogWithElasticSearch();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void ConfigureSerilogWithElasticSearch()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                            .Enrich.FromLogContext()
+                            .Enrich.WithExceptionDetails()
+                            .Enrich.WithMachineName()
+                            .WriteTo.Debug()
+                            .WriteTo.Console()
+                            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearchUrl"]))
+                            {
+                                AutoRegisterTemplate = true,
+                                IndexFormat = $"Service_{ Assembly.GetExecutingAssembly().GetName().Name }"
+                            })
+                            .ReadFrom.Configuration(configuration)
+                            .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +42,7 @@ namespace Serilog.Elastic.Kibana.WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
